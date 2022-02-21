5f6cbfc84ce99f47831a978bc058cd5dbbcceff4
Angelurbina4 <angelurbina44@gmail.com>
2022-02-21 14:41:19 -0300

    commit final

---
+++ bucles.js
@@ -0,0 +1,46 @@
+// Todos los valores impares del 1 al 20
+
+// for (var i=1; i<=20; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+// Todos los valores que sean divisibles de manera uniforme por 3 del 100 al 0
+
+// for (var i=100; i>=0; i--) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// Imprima los valores en esta secuencia  4, 2.5, 1, -0.5, -2, -3.5
+// for (var i=4.0; i>=-3.5; i-=1.5) {
+//   console.log(i)
+//   }
+
+// var x=4
+// while(x>=-3.5) {
+//   console.log(x);
+//   x-=1.5;
+// }
+
+// for (let index = 4; index > -3.5; index-=1.5) {
+//   console.log(index);
+// }
+
+// Deberíamos obtener 5050 al final
+
+// var sum = 0;
+// for(let x = 1; x<=100; x++) {
+//   sum+=x
+//   console.log(sum)
+// }
+
+//Deberíamos obtener  479001600al final.
+
+// var produc = 1;
+// for(let x = 1; x<=12; x++) {
+//   produc*=x
+//   console.log(produc)
+// }
